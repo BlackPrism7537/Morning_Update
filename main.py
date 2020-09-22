d343c3ca1dbe33f2e3d2b3c2c286ec43b52d6246
Zachery Barcoe <z.barcoe@gmail.com>
2020-09-22 23:50:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
-import news_feed as nf
-import weather_feed as wf
-import pprint 
-import json
+from json import load
+from weather_feed import getWeather
+from news_feed import getArticles
 from os.path import dirname, abspath, join
 from textwrap import wrap
-from datetime import date, datetime
+from datetime import datetime
 
-path = dirname(dirname(abspath(__file__)))
+with open(join(dirname(abspath(__file__)), 'settings.json'), "r") as f:
+    settings = load(f)
 
-with open(join(path, 'settings.json'), "r") as f:
-    settings = json.load(f)
 
-current_date = date.today()
-current_time = datetime.now()
-print(current_date.strftime("%d %B, %Y") + " - " + current_time.strftime("%H:%M:%S"))
+def time():
+    current_date = datetime.today()
+    current_time = datetime.now()
+    print(current_date.strftime("%d %B, %Y") + " - " + current_time.strftime("%H:%M:%S"))
  
-weather_data = wf.getWeather(settings['location'], settings['owmapi-key'])
-waether_loc  = weather_data['name']
-weather_temp = str(int(weather_data['main']['temp']))
-weather_desc = weather_data['weather'][0]['description']
 
-print("{0}: {1}ºc : {2}".format(waether_loc, weather_temp, weather_desc))
+def weather():
+    weather_data = getWeather(settings['location'], settings['owmapi-key'])
+    waether_loc  = weather_data['name']
+    weather_temp = int(weather_data['main']['temp'])
+    weather_desc = weather_data['weather'][0]['description']
 
-print("\n------------------News------------------")
+    print("{0}: {1}ºc : {2}".format(waether_loc, weather_temp, weather_desc))
 
-for keyphrase in settings['keyphrases']:
-    print("\n" + keyphrase + ":")
-    articles = nf.getArticles(keyphrase, settings['newsapi-key'])
+
+def news():
+    print("\n------------------News------------------")
+
+    for keyphrase in settings['keyphrases']:
+        print("\n" + keyphrase + ":")
+        articles = getArticles(keyphrase, settings['newsapi-key'])
+        
+        for article in articles:
+            text = wrap(article['title'], 40)
+            for line in text:
+                print (line)
+            print("({0})".format(article['source']['name']))
+
+
+if __name__ == "__main__":
+    time()
+    weather()
+    news()
     
-    for article in articles:
-        text = wrap(article['title'], 40)
-        for line in text:
-            print (line)
-        print("({0})".format(article['source']['name']))
